94b1b62123ee96a75c9fb57e8c23fde3cec6baa1
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-22 20:52:06 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,7 @@ def page_not_found(error):
     """
     handler error 404
     """
-    return (jsonify(error="Not found"), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
