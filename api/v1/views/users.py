d779780cf48a51eee196d17ceda26fd2f8633a8a
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-23 13:39:56 +0300

    adding imports

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+"""
+ New view for Users objects that handles all default RestFul API actions.
+"""
+from models.user import User
+from models import storage
+from flask import Flask, request, jsonify, abort
+from api.v1.views import app_views
