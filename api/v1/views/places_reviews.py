b7b7a807ee4353cd45d51a4600336184e88871c6
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-23 13:40:54 +0300

    add imports

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/python3
+"""
+ New view for Reviews objects that handles all default RestFul API actions.
+"""
+from models.place import Place
+from models.review import Review
+from models import storage
+from flask import Flask, request, jsonify, abort
+from api.v1.views import app_views
