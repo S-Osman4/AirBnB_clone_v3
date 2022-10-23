a392ac58c8aa23d46b7fb88013930bd63737f494
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-23 13:42:31 +0300

    add imports

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/python3
+"""
+objects that handles all default RestFul API actions:
+"""
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.state import State
