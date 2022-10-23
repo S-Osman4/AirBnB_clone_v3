ef73f88e704b604e8786ed72abc0a5206267157a
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-23 13:41:39 +0300

    add imports

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/python3
+"""
+ New view for Places objects that handles all default RestFul API actions.
+"""
+from models.place import Place
+from models.city import City
+from models import storage
+from flask import Flask, request, jsonify, abort
+from api.v1.views import app_views
