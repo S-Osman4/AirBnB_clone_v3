f799a96fea5c2f64cc5a3340f58158194efb1ebc
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-24 13:31:31 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """create API with flask"""
 
-from flask import Flask
-from flask import jsonify, make_response
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
-from models.engine import *
 from os import getenv
 from flask_cors import CORS
-
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 
 
 @app.teardown_appcontext
